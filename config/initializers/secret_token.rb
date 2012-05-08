3caa6cd18bae19e6843095b0b71ef3d9e67c71db
Jonathan Mumm <mumm@tokbox.com>
2012-05-07 17:20:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Covify::Application.config.secret_token = '338ab64a7072ecd5d708e387b964303481d3c5d3b003fc66fd966e835a414964534004fe4a49d40d010b32313f256fe8f7b02275ecdbd6a508914d0259fe675b'
